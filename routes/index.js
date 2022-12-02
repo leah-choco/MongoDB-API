cfcf63e4101c9104cc0c8b0025d66b1a004d5790
Your Name <you@example.com>
2022-12-02 11:21:18 -0600

    Creating routes folders, setting up indexes

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const thoughtsRoutes = require("./thoughtsRoutes");
+
+router.use("/users", userRoutes);
+
+router.use("/thoughts", thoughtsRoutes);
+
+module.exports = router;
